b359eabbbd7f4163493041096b433ab8797a777a
Shaik Kousarunnisa <skarshi67@gmail.com>
2026-06-18 09:53:37 +0530

    Update IEP SQL PROJECT.sql

--- IEP SQL PROJECT.sql
+++ IEP SQL PROJECT.sql
@@ -1,7 +1,14 @@
+### DATABASE CREATION :
+  
 create database job_market;
+
+### USING AND SELECTING OF DATASET :
+  
 use job_market;
 select * from job_market_dataset;
 
+### SQL QUERIES :
+  
 select company,year,count(*) as total_jobs
 from job_market_dataset
 group by company,year;
@@ -57,3 +64,9 @@ having count(*) > 10;
 select company,max(year) as latest_year
 from job_market_dataset
 group by company;
+
+
+### CONCLUSION :
+
+Developed an SQL-based Job Market Analysis project by solving 10 business-driven queries 
+to identify hiring trends, top recruiters, job category demand, and location-wise employment opportunities.
